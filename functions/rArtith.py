76232e3a169208c55c86b4f883d38c470b510277
Lucas Hygidio <luhygidio.lh@gmail.com>
2026-09-07 19:18:24 -0300

    IMplementando os nomes especificos de arith

--- functions/rArtith.py
+++ functions/rArtith.py
@@ -1,5 +1,12 @@
 import re
 def rArith(value : str):
+  tokens = {
+    "+": "PLUS",
+    "-": "MINUS",
+    "*": "STAR",
+    "/": "SLASH"
+  } 
+
   if (re.fullmatch(r"[+*/-]", value)):
-    return "Arith"
+    return tokens.get(value)
   return None
